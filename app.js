b457993f34bcc7887374fe7d6a35770ca0063296
Erik <esandqvist04@gmail.com>
2025-02-22 22:47:01 +0100

    off to a start and much styling done

---
+++ app.js
@@ -0,0 +1,6 @@
+const ad = document.querySelector('.song');
+const playing = document.querySelector('.fa-play');
+const pauses = document.querySelector('.fa-pause');
+const forw = document.querySelector('.fa-forward-step');
+const ttl = document.querySelector('.title');
+const art_img = document.querySelector('#artist');
